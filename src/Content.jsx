0faeed6020b5e205f0de4b6b33bf5e2085a0d971
jonnywintech <jonnywintech@gmail.com>
2023-11-03 02:33:04 +0100

    still need fixing for login

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
+import Login from './pages/Login';
 
 const Content = () => {
   return (
     <>
         <Routes>
             <Route path="/" element={<Home />}/>
-            {/* <Route path="/about-us" element={<OurCompany />}/>
-            <Route path="/locations" element={<Locations />}/>
+            <Route path="/Login" element={<Login />}/>
+            {/* <Route path="/locations" element={<Locations />}/>
             <Route path="/contact-us" element={<Contact />}/>
             <Route path="/web-design" element={<WebDesign />}/>
             <Route path="/app-design" element={<AppDesign />}/>
